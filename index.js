96fd542fbe9d0237e5d341e58599d9ece6042bb6
gideonekekeke <gideonekeke64@gmail.com>
2022-04-13 18:17:20 +0100

    good

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 require('dotenv').config()
 require("./config/db.js")
 const express = require("express")
-const app = express()
 const cors = require("cors")
+const app = express()
+app.use(express.json())
+app.use(cors())
+
 
 const myRouter = require("./Router/router")
 const myRouter2 = require("./Router/router2")
@@ -11,12 +14,11 @@ const port = process.env.PORT || 4040
 
 
 
-// app.get("/", (req, res)=>{
-//    res.send("port is running well well well")
-// })
+app.get("/", (req, res)=>{
+   res.send("port is running well well well")
+})
 
-app.use(express.json())
-app.use(cors())
+
 app.use("/api", require("./Router/router2"))
 app.use("/api", myRouter)
 
