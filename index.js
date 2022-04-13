97f3834fd3fa654b22565dae3588c495d836eb4c
AnyamahNdidi <theo4felix@gmail.com>
2022-04-13 17:52:46 +0100

    the process

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+
+require('dotenv').config()
 require("./config/db.js")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const dotenv = require("dotenv")
+
 const myRouter = require("./Router/router")
 const myRouter2 = require("./Router/router2")
 const port = process.env.PORT || 4040
 
-dotenv.config()
+
 
 // app.get("/", (req, res)=>{
 //    res.send("port is running well well well")
